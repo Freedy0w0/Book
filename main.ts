23e7c591792f3da8b1732cffff977530ff473522
Freedy0w0 <Freedy0w0@users.noreply.github.com>
2023-07-29 21:11:23 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,12 @@
+let 我的變數 = randint(1, 6)
+basic.showNumber(我的變數)
+if (我的變數 == 6) {
+    basic.showIcon(IconNames.Happy)
+} else if (我的變數 == 3) {
+    basic.showIcon(IconNames.Asleep)
+} else {
+    basic.showIcon(IconNames.Sad)
+}
+basic.forever(function () {
+	
+})
